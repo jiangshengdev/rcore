d969a424316ada8fdfac30934dd6adac22f45703
Jiang Sheng <jiangshengdev@outlook.com>
2025-05-16 20:49:53 +0800

    fmt

--- scripts/generate-memory.sh
+++ scripts/generate-memory.sh
@@ -14,10 +14,10 @@ command -v dot >/dev/null 2>&1 || {
 
 # 生成 light 主题
 mkdir -p scripts/memory/light
-python3 -m scripts.memory.generate_memory_dot scripts/memory/mem.txt --theme light > scripts/memory/light/memory.dot
+python3 -m scripts.memory.generate_memory_dot scripts/memory/mem.txt --theme light >scripts/memory/light/memory.dot
 dot -Tsvg:cairo scripts/memory/light/memory.dot -o scripts/memory/light/memory.svg
 
 # 生成 dark 主题
 mkdir -p scripts/memory/dark
-python3 -m scripts.memory.generate_memory_dot scripts/memory/mem.txt --theme dark > scripts/memory/dark/memory.dot
+python3 -m scripts.memory.generate_memory_dot scripts/memory/mem.txt --theme dark >scripts/memory/dark/memory.dot
 dot -Tsvg:cairo scripts/memory/dark/memory.dot -o scripts/memory/dark/memory.svg
