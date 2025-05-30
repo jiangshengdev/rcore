842be3d2026597eaee7ea684fe6d59460bc33b0c
Jiang Sheng <jiangshengdev@outlook.com>
2025-05-30 09:22:10 +0800

    fmt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs"
-          >
+          <Link className="button button--secondary button--lg" to="/docs">
             Tutorial
           </Link>
           <Link className="button button--secondary button--lg" to="/blog">
